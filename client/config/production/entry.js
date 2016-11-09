61a76b8f68c135b6f0f8e83a49ac26b39ff802e8
Danyel Cabello <danyel.nerv@gmail.com>
2016-11-09 08:38:50 -0600

    Enable react in client side in production environment

--- client/config/production/entry.js
+++ client/config/production/entry.js
@@ -1,2 +1,7 @@
+import createHistory from 'history/createBrowserHistory'
+
 import 'bootstrap/dist/js/bootstrap.min';
 import 'client/lib/template';
+import app from 'client/app';
+
+app(createHistory);
